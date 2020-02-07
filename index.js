01175b6480cb4b0a150892e78c48025ba1d142b6
abeluiux <abeluiux@gmail.com>
2020-02-07 15:14:28 -0500

    Permite actualizar IP
    - index.js -c --config

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ const path = require('path')
 const os = require('os')
 const inquirer = require('inquirer')
 const ipRegex = require('ip-regex')
-// const bcrypt = require('bcryptjs')
 const Datastore = require('nedb')
 const express = require('express')
 const proxy = require('http-proxy-middleware')
-//
+const program = require('commander')
 const tmp = path.join(os.tmpdir(), 'sp-ic')
 const db = new Datastore({ filename: tmp })
+
+program.option('-c, --config')
+program.parse(process.argv)
+
 db.loadDatabase(function (err) {
   if (err) {
     console.log('Iniciando Servidor: Error 001') // BD imposible de cargar
+    return false
+  }
+  // CMD
+  if (program.config) {
+    inquirer
+    .prompt([ { type: 'list', name: 'opcion', message: '¿Qué desea hacer?', choices: [{ name: 'Cambiar IP' }, { name: 'Salir' }] } ])
+    .then(({ opcion }) => {
+      if (opcion === 'Cambiar IP') {
+        question()
+      }
+    })
+  
     return false
   }
 
@@ -35,14 +50,29 @@ function question() {
   .prompt([ { type: 'input', name: 'IP', message: 'Ingresar IP:' } ])
   .then(({ IP }) => {
     if ( ipRegex({exact: true}).test(IP)) {
-      // var hash = bcrypt.hashSync(IP, 10)
-      db.insert({ ip: IP }, function (err) {
+      db.find({}, function (err, docs) {
         if (err) {
           console.log('Iniciando Servidor: Error 003') // BD imposible de leer 
           return false
         }
-        runServer(IP)
-      });
+        if (docs.length) {
+          db.update({ ip: docs[0].ip }, { ip: IP }, {}, function (err) {
+            if (err) {
+              console.log('Iniciando Servidor: Error 004') // BD imposible de leer 
+              return false
+            }
+            runServer(IP)
+          })
+        } else {
+          db.insert({ ip: IP }, function (err) {
+            if (err) {
+              console.log('Iniciando Servidor: Error 005') // BD imposible de leer 
+              return false
+            }
+            runServer(IP)
+          })
+        }
+      })
     } else {
       console.log('IP invalida!')
       question()
@@ -55,15 +85,12 @@ function runServer(IP) {
     target: 'http://' + IP + ':4688',
     changeOrigin: true,
     logProvider: (provider) => { return provider  }
-    // onProxyRes: (proxyRes, req, res) => {
-    //   console.log(req.url)
-    // }
   }
   const app = express()
 
   app.use('/', proxy(options))
   console.log('...')
-  console.log('v1.0.0')
+  console.log('v1.1.0')
   console.log('Iniciando Servidor: Ok')
   app.listen(4688)
 }
