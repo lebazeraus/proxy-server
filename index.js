f0ae928a5ad49bdf705d9bffb5153c04713aa922
abeluiux <abeluiux@gmail.com>
2020-02-07 13:52:16 -0500

    Almacenamiento de IP

--- index.js
+++ index.js
@@ -1,8 +1,69 @@
+const path = require('path')
+const os = require('os')
+const inquirer = require('inquirer')
+const ipRegex = require('ip-regex')
+// const bcrypt = require('bcryptjs')
+const Datastore = require('nedb')
 const express = require('express')
 const proxy = require('http-proxy-middleware')
- 
-const app = express()
- 
-app.use('/', proxy({ target: 'http://26.92.178.124', changeOrigin: true }))
+//
+const tmp = path.join(os.tmpdir(), 'sp-ic')
+const db = new Datastore({ filename: tmp })
+db.loadDatabase(function (err) {
+  if (err) {
+    console.log('Iniciando Servidor: Error 001') // BD imposible de cargar
+    return false
+  }
 
-app.listen(3088)
+  db.find({}, function (err, docs) {
+    if (err) {
+      console.log('Iniciando Servidor: Error 002') // BD imposible de leer
+      return false
+    }
+
+    if (docs.length) {
+      // Run server
+      runServer(docs[0].ip)
+    } else {
+      question()
+    }
+  })
+})
+
+function question() {
+  inquirer
+  .prompt([ { type: 'input', name: 'IP', message: 'Ingresar IP:' } ])
+  .then(({ IP }) => {
+    if ( ipRegex({exact: true}).test(IP)) {
+      // var hash = bcrypt.hashSync(IP, 10)
+      db.insert({ ip: IP }, function (err) {
+        if (err) {
+          console.log('Iniciando Servidor: Error 003') // BD imposible de leer 
+          return false
+        }
+        runServer(IP)
+      });
+    } else {
+      console.log('IP invalida!')
+      question()
+    }
+  })
+}
+
+function runServer(IP) {
+  const options = {
+    target: 'http://' + IP + ':4688',
+    changeOrigin: true,
+    logProvider: (provider) => { return provider  }
+    // onProxyRes: (proxyRes, req, res) => {
+    //   console.log(req.url)
+    // }
+  }
+  const app = express()
+
+  app.use('/', proxy(options))
+  console.log('...')
+  console.log('v1.0.0')
+  console.log('Iniciando Servidor: Ok')
+  app.listen(4688)
+}
